7b778bb15b4ff89a6f902236537e2d118281d9b1
Mathias Winkel <mathias.winkel@merckgroup.com>
2024-11-11 21:03:02 +0100

    add comment on Keras 2 vs 3 env var

--- decimer_segmentation/__init__.py
+++ decimer_segmentation/__init__.py
@@ -11,6 +11,9 @@ please raise an issue on our GitHub repository.
 """
 
 import os
+
+# Use Keras 2 instead of 3, see
+# https://blog.tensorflow.org/2024/03/whats-new-in-tensorflow-216.html
 os.environ['TF_USE_LEGACY_KERAS'] = "1"
 del os
 
